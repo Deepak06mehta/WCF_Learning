0ca15400cf98a704543365d453527a34a1206e6f
Deepak06mehta <deepak.06mehta@gmail.com>
2021-08-02 03:28:45 +0530

    WCF Service
    WCF Service

---
+++ WCF/WCFHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCFHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            using(ServiceHost host =new ServiceHost(typeof(WCF.Service.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("open Host @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
